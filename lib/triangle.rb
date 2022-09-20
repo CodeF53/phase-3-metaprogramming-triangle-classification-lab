1f10c359f7451a3501cd0335ea024aed15ea987f
ChaseWelsh <fseusb@gmail.com>
2022-09-20 14:28:16 -0600

    commit tree 28e3b7ee1270bf1b7b5efa162105b7c67729c9eb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  def initialize sideA, sideB, sideC
+    @a = sideA
+    @b = sideB
+    @c = sideC
+  end
+
+  def kind
+    if valid?
+      if a == b && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      nil
+    end
+  end
+
+  def valid?
+    if [a,b,c].all? { |length| length > 0 }
+      if a+b>c && b+c>a && c+a>b
+        return true
+      else
+        # begin
+          raise TriangleError
+        # rescue TriangleError => exception
+        #   puts "The sum of the lengths of any two sides of a triangle should always exceed the length of the third side"
+        # end
+      end
+    else
+      # begin
+        raise TriangleError
+      # rescue TriangleError => exception
+      #   puts "Each side of a triangle must be larger than 0."
+      # end
+    end
+    return false
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The sum of the lengths of any two sides of a triangle should always exceed the length of the third side\nEach side of a triangle must be larger than 0."
+    end
+  end
 end
